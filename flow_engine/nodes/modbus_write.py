7557d0061a5df0c3b45ddca0a29d7873f947e724
SaeedHotshotBj <saeedhotshotbj@gmail.com>
2026-09-05 09:10:26 +0330

    Revert unused direct ModbusWrite backend change

--- flow_engine/nodes/modbus_write.py
+++ flow_engine/nodes/modbus_write.py
@@ -1,210 +1,33 @@
 from pymodbus.client.sync import ModbusTcpClient
 
-
-
-
-
 class ModbusWrite:
-
-
-
     def __init__(self, config):
-
         self.config = config
 
-
-
-
-
-
-    def execute(
-
-        self,
-
-        data=None
-
-    ):
-
-
-
-        print(
-
-            "Modbus Write..."
-
-        )
-
-
-
-
-
-
-        ip = self.config.get(
-
-            "ip"
-
-        )
-
-
-
-        port = self.config.get(
-
-            "port",
-
-            502
-
-        )
-
-
-
-        slave = self.config.get(
-
-            "slave",
-
-            1
-
-        )
-
-
-
-        register = self.config.get(
-
-            "register"
-
-        )
-
-
-
-        value = self.config.get(
-
-            "value"
-
-        )
-
-        if data is not None and "Value" in data:
-
-            value = data.get(
-
-                "Value"
-
-            )
-
-
-
+    def execute(self, data=None):
+        print("Modbus Write...")
+        ip = self.config.get("ip")
+        port = self.config.get("port", 502)
+        slave = self.config.get("slave", 1)
+        register = self.config.get("register")
+        value = self.config.get("value")
 
         if register is None:
-
-            print(
-
-                "Register not configured"
-
-            )
-
-
+            print("Register not configured")
             return data
-
-
-
-        if value is None:
-
-            print(
-
-                "Value not configured"
-
-            )
-
-            return data
-
-
-
 
         try:
-
-
-
-            client = ModbusTcpClient(
-
-                ip,
-
-                port=port
-
-            )
-
-
-
-
-
+            client = ModbusTcpClient(ip, port=port)
             if client.connect():
-
-
-
-                result = client.write_register(
-
-                    register,
-
-                    value,
-
-                    unit=slave
-
-                )
-
-
-
-
+                result = client.write_register(register, value, unit=slave)
                 client.close()
-
-
-
-
-
                 if result.isError():
-
-                    print(
-
-                        "PLC Write Error"
-
-                    )
-
-
-
+                    print("PLC Write Error")
                 else:
-
-                    print(
-
-                        "PLC Write Successful"
-
-                    )
-
-
-
-
+                    print("PLC Write Successful")
             else:
-
-                print(
-
-                    "PLC Connection Failed"
-
-                )
-
-
-
-
-
+                print("PLC Connection Failed")
         except Exception as e:
-
-
-
-            print(
-
-                "Modbus Error:",
-
-                e
-
-            )
-
-
-
-
-
-
+            print("Modbus Error:", e)
 
         return data
